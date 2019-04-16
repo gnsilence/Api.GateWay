c3ad8f1d30e5cddb00c16ab9bb45a4bbc83a73a3
nan gong <gnsilence@outlook.com>
2019-04-16 18:19:36 +0800

    修改

--- Api.GateWay/Filter/RequestFilterPlugin.cs
+++ Api.GateWay/Filter/RequestFilterPlugin.cs
@@ -39,8 +39,10 @@ namespace Api.GateWay.Filter
                     Method = e.Request.Method,
                     RequestUrL = e.Request.BaseUrl
                 };
-                //UseCachePolicyWithLocking.Check(new GenericKey(ipaddress));
-                var reslut = apiRequestPolicy.Check(httpRequest);
+                //缓存限流
+                var reslut =UseCachePolicyWithLocking.Check(new GenericKey(ipaddress));
+                //redis 限流
+                //var reslut = apiRequestPolicy.Check(httpRequest);
 
                 if (reslut.IsThrottled)
                 {
@@ -59,49 +61,49 @@ namespace Api.GateWay.Filter
 
         public void Init(Gateway gateway, Assembly assembly)
         {
-            
+
         }
 
         #region 使用redis限流
-        /// <summary>
-        ///使用redis
-        /// </summary>
-        static RedisThrottleRepository repository = new RedisThrottleRepository(ConfigSettings.Instance.RedisServerConnectionString);
-        /// <summary>
-        /// 使用redis限流
-        /// </summary>
-        HttpThrottlePolicy<HttpRequestKey> apiRequestPolicy = new HttpThrottlePolicy<HttpRequestKey>(repository) // Pass in the Redis repository
-        {
-            Name = "limit  Requests by redis",
-            Prefixes = new[] { "requests" },
-            //白名单
-            IgnoreList = new[] { "127.0.0.1", "::1" },
-            //PerSecond = 1000, // 每秒多少次
-            //可以组合限流
-            Limiters = new Limiter[]
-           {
-               //限制不锁定
-                new Limiter()
-               .Limit(100)                        // 限制访问数 100 requests,
-               .Over(TimeSpan.FromSeconds(1))     // 时间,
-               ,
-               new Limiter()
-               .Limit(10000)
-               .Over(TimeSpan.FromMinutes(5))//5分钟超过会被锁定
-               .LockFor(TimeSpan.FromMinutes(5))
-               //一个小时内访问超过被锁定1个小时不得访问
-               ,new Limiter()
-               .Limit(100000)
-               .Over(TimeSpan.FromHours(1))
-               .LockFor(TimeSpan.FromHours(1))
-               ,
-                    //一天内访问超过被锁定3天内不得访问接口
-                new Limiter()
-               .Limit(300000)
-               .Over(TimeSpan.FromDays(1))
-               .LockFor(TimeSpan.FromDays(3))
-           }
-        };
+        ///// <summary>
+        /////使用redis
+        ///// </summary>
+        //static RedisThrottleRepository repository = new RedisThrottleRepository(ConfigSettings.Instance.RedisServerConnectionString);
+        ///// <summary>
+        ///// 使用redis限流
+        ///// </summary>
+        //HttpThrottlePolicy<HttpRequestKey> apiRequestPolicy = new HttpThrottlePolicy<HttpRequestKey>(repository) // Pass in the Redis repository
+        //{
+        //    Name = "limit  Requests by redis",
+        //    Prefixes = new[] { "requests" },
+        //    //白名单
+        //    IgnoreList = new[] { "127.0.0.1", "::1" },
+        //    //PerSecond = 1000, // 每秒多少次
+        //    //可以组合限流
+        //    Limiters = new Limiter[]
+        //   {
+        //       //限制不锁定
+        //        new Limiter()
+        //       .Limit(100)                        // 限制访问数 100 requests,
+        //       .Over(TimeSpan.FromSeconds(1))     // 时间,
+        //       ,
+        //       new Limiter()
+        //       .Limit(10000)
+        //       .Over(TimeSpan.FromMinutes(5))//5分钟超过会被锁定
+        //       .LockFor(TimeSpan.FromMinutes(5))
+        //       //一个小时内访问超过被锁定1个小时不得访问
+        //       ,new Limiter()
+        //       .Limit(100000)
+        //       .Over(TimeSpan.FromHours(1))
+        //       .LockFor(TimeSpan.FromHours(1))
+        //       ,
+        //            //一天内访问超过被锁定3天内不得访问接口
+        //        new Limiter()
+        //       .Limit(300000)
+        //       .Over(TimeSpan.FromDays(1))
+        //       .LockFor(TimeSpan.FromDays(3))
+        //   }
+        //};
         #endregion
 
         #region 使用缓存限流
@@ -118,7 +120,7 @@ namespace Api.GateWay.Filter
                     new Limiter()
                     .Limit(200)                        // 限制访问数 200 requests,
                     .Over(TimeSpan.FromSeconds(1))     // 限制时间
-                    .LockFor(TimeSpan.FromMinutes(5))  // 锁定时间，锁定时间内不允许访问
+                    .LockFor(TimeSpan.FromMinutes(1))  // 锁定时间，锁定时间内不允许访问
             }
         };
         #endregion
